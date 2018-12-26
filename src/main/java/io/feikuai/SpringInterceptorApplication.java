35e1d6e7e3d1c88f9d8d16ce52aa4c816dc54518
liudongxu <562430258@qq.com>
2018-12-26 11:51:01 +0800

    spring boot 框架

---
+++ src/main/java/io/feikuai/SpringInterceptorApplication.java
@@ -0,0 +1,14 @@
+package io.feikuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInterceptorApplication.class, args);
+	}
+
+}
+
